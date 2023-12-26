f1575afc41039bf329146a9d592c9cf7e34081a1
mansour365 <54915792+mansour365@users.noreply.github.com>
2023-12-26 18:43:24 -0500

    Added missing css for edit card mode + added missing import statements + improved readability of code

--- src/components/AddBtn.js
+++ src/components/AddBtn.js
@@ -1,15 +1,16 @@
 import React from 'react';
-import CreateCard from './CreateCard';
 
 function AddBtn(){
     
     function handleClick(){
-        <CreateCard />
+        alert("you clicked"); 
     }
     
-    return (<div className="addBtn" type="button" onClick={handleClick}>
-                    <span className="material-symbols-outlined">add</span>
-           </div>);
+    return (
+        <div className="addBtn" type="button" onClick={handleClick}>
+            <span className="material-symbols-outlined">add</span>
+        </div>
+    );
 }
 
 export default AddBtn;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+import reportWebVitals from './reportWebVitals';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
