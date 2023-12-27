5c987bcae55e3c3eb58e394d77d92a0ce12dd4b2
mansour365 <54915792+mansour365@users.noreply.github.com>
2023-12-27 15:23:43 -0500

    Moved journalData Outside of components file + removed Journal Wrapper + Cards now starting to display based on number of objects in array + card shadow
    JournalWrapper seemed to be only adding to complexity for no reason

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+/*Import the css*/
 import './App.css';
-import JournalWrapper from './components/JournalWrapper';
+/*Import the array of Journal data*/
+import {entries} from './journalData';
+/*Import components*/
+import AddBtn from './components/AddBtn';
+import Card from './components/Card';
+
 
 function App() {
   return (
     <div className="App">
-      <JournalWrapper />
+
+      <div id="title">Journal</div>
+
+      <div id = "addBtn"> <AddBtn /> </div>
+
+      {/*map objects from journalData put each object into an instance of Card*/}
+      {entries.map(entry => <Card entryObject = {entry}/> )}
+
+      
     </div>
   );
 }
